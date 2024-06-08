b10cc6c41db11811694398d69a4dc30d8532b404
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-06-08 22:23:39 +0200

    Add files via upload

---
+++ Java-30-Days-Of-Code-updated-/DAY 3/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        if (n % 2 != 0)
+        {
+            System.out.println("Weird");
+        }
+
+        else if (n % 2 == 0 && n >= 2 && n <= 5)
+        {
+            System.out.println("Not Weird");
+        }
+
+        else if (n % 2 == 0 && n >= 6 && n <= 20)
+        {
+            System.out.println("Weird");
+        }
+
+        else if (n % 2 == 0 && n > 20)
+        {
+            System.out.println("Not Weird");
+        }
+    }
+}
