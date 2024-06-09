f171b682c2063565b8d74b4ff6a7a4261837afc7
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-06-09 23:18:30 +0200

    Update Solution.java
    My lecturer complained that my code was kinda difficult to fathom, so I had to make it simpler to grasp.

--- Java-30-Days-Of-Code-updated-/DAY 3/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 3/src/Solution.java
@@ -2,28 +2,34 @@ import java.util.Scanner;
 
 public class Solution
 {
-    public static void main(String[] args)
+    /**
+     * If n is odd, print Weird
+     * If n is even and in the inclusive range of 2 to 5, print Not Weird
+     * If n is even and in the inclusive range of 6 to 20, print Weird
+     * If n is even and greater than 20, print Not Weird
+     */
+
+    public static void main(String args[])
     {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.close();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
 
         if (n % 2 != 0)
         {
             System.out.println("Weird");
         }
 
-        else if (n % 2 == 0 && n >= 2 && n <= 5)
+        else if (n % 2 == 0 && (n >= 2 && n <= 5))
         {
             System.out.println("Not Weird");
         }
 
-        else if (n % 2 == 0 && n >= 6 && n <= 20)
+        else if (n % 2 == 0 && (n >= 6 && n <= 20))
         {
             System.out.println("Weird");
         }
 
-        else if (n % 2 == 0 && n > 20)
+        else
         {
             System.out.println("Not Weird");
         }
