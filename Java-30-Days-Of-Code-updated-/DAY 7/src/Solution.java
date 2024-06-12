8e705699f5cedb4abd6e15941adc41618543660e
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-06-12 22:12:36 +0200

    Update Solution.java
    Made changes

--- Java-30-Days-Of-Code-updated-/DAY 7/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 7/src/Solution.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
-        Scanner scanner = new Scanner(System.in);
-
-        // Ask the user for the number of elements
-        int n = scanner.nextInt();
-
-        int[] arr = new int[n];
-
-        // Input the elements from the user
-        for (int i = 0; i < n; i++)
+        Scanner scan = new Scanner(System.in);
+        int arraySize = scan.nextInt();
+        scan.nextLine();
+        if (arraySize >= 1 && arraySize <= 1000)
         {
-            arr[i] = scanner.nextInt();
+            int[] array = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+                {
+                    array[i] = scan.nextInt();
+                }
+
+                for (int j = arraySize - 1; j >= 0; j--)
+                {
+                    System.out.print(array[j] + " ");
+                }
         }
 
-        // Print the elements in reverse order
-        for (int i = n - 1; i >= 0; i--)
+        else
         {
-            System.out.print(arr[i] + " ");
+            System.out.println("Invalid Input");
         }
-
-        scanner.close(); // Close the scanner
     }
 }
