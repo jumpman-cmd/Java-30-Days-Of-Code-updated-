58f14e1d5d58d6cb11df9c376a8cde3541c374b6
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-06-12 12:26:56 +0200

    Update Solution.java
    Made the code more simpler.

--- Java-30-Days-Of-Code-updated-/DAY 6/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 6/src/Solution.java
@@ -2,35 +2,78 @@ import java.util.Scanner;
 
 public class Solution
 {
+    String s;
 
-    public static void main(String[] args)
+    // Getters and setters
+    public String getS()
     {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        scanner.nextLine();  // consume the newline
+        return this.s;
+    }
 
-        for (int i = 0; i < T; i++)
+    public void setS(String newS)
+    {
+        this.s = newS;
+    }
+
+   /** public Solution(String enteredS)
+    {
+        if (this.s.length() >= 2 && this.s.length() <= 10000)
         {
-            String input = scanner.nextLine();
-            StringBuilder evenIndexed = new StringBuilder();
-            StringBuilder oddIndexed = new StringBuilder();
+            s = enteredS;
+        }
 
-            for (int j = 0; j < input.length(); j++)
+        else
+        {
+            System.out.println("Invalid input. Rerun the program and enter appropriate data.");
+        }
+    } **/
+
+
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int T = scan.nextInt();
+        scan.nextLine();
+        if (T >= 1 && T <= 10)
+        {
+            StringBuilder results = new StringBuilder();
+            for (int z = 0; z < T; z++)
             {
-                if (j % 2 == 0)
+                String s = scan.nextLine();
+                if (s.length() >= 2 && s.length() <= 10000)
                 {
-                    evenIndexed.append(input.charAt(j));
+                    StringBuilder evenChars = new StringBuilder();
+                    StringBuilder oddChars = new StringBuilder();
+                    for (int j = 0; j < s.length(); j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            evenChars.append(s.charAt(j));
+                        }
+
+                        else
+                        {
+                            oddChars.append(s.charAt(j));
+                        }
+                    }
+
+                    results.append(evenChars.toString()).append(" ").append(oddChars.toString()).append("\n");
                 }
 
                 else
                 {
-                    oddIndexed.append(input.charAt(j));
+                    System.out.println("\nInvalid input. Rerun the program and enter appropriate data. Enter in a range 2 to 10000 characters.");
+                    return; // Terminate the program
                 }
             }
 
-            System.out.println(evenIndexed.toString() + " " + oddIndexed.toString());
+            System.out.println(results.toString());
         }
 
-        scanner.close();
+        else
+        {
+            System.out.println("\nInvalid Input. Enter in a range 1 to 10. Rerun the program and try again.");
+        }
     }
 }
