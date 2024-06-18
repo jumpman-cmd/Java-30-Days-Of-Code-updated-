a413c94e796cdf116777cc1adcd2a54cc3399547
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-06-18 11:25:11 +0200

    Add files via upload

---
+++ Java-30-Days-Of-Code-updated-/DAY 14/src/Solution.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    // Constructor to initialize the elements array
+    public Difference(int[] elements)
+    {
+        this.elements = elements;
+    }
+
+    // Method to compute the maximum difference
+    public void computeDifference()
+    {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int element : elements)
+        {
+            if (element < min)
+            {
+                min = element;
+            }
+
+            if (element > max)
+            {
+                max = element;
+            }
+        }
+
+        maximumDifference = max - min;
+    }
+
+} // End of Difference class
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
