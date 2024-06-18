6c7f52df0e69c3f2ae4e6088bb6eec8701cde0ea
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-06-18 18:15:41 +0200

    Update Solution.java
    Updated the code.

--- Java-30-Days-Of-Code-updated-/DAY 9/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 9/src/Solution.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class Solution
 {
-    public static void main(String[] args)
+    public static int Factorial(int n)
     {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.close();
-
-        System.out.println(factorial(n));
-    }
-
-    public static int factorial(int n)
-    {
-        if (n <= 1)
+        // Base Case
+        if (n <= 1) // multi identity
         {
             return 1;
         }
 
+        // Recursive Case
         else
         {
-            return n * factorial(n - 1);
+            return n * Factorial (n - 1);
         }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(Factorial(n));
     }
 }
