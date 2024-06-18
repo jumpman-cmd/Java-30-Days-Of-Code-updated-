e78a77f2360a2b357a7bb766d95edbaceb1ab899
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-06-18 13:19:48 +0200

    Update Solution.java
    Updated the code, and made it simpler.

--- Java-30-Days-Of-Code-updated-/DAY 7/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 7/src/Solution.java
@@ -7,24 +7,25 @@ public class Solution
         Scanner scan = new Scanner(System.in);
         int arraySize = scan.nextInt();
         scan.nextLine();
+
         if (arraySize >= 1 && arraySize <= 1000)
         {
+            int i = 0;
             int[] array = new int[arraySize];
+            for (i= 0; i < arraySize; i++)
+            {
+                array[i] = scan.nextInt();
+            }
 
-            for (int i = 0; i < arraySize; i++)
-                {
-                    array[i] = scan.nextInt();
-                }
-
-                for (int j = arraySize - 1; j >= 0; j--)
-                {
-                    System.out.print(array[j] + " ");
-                }
+            for (int j = arraySize - 1; j >= 0; j--)
+            {
+                System.out.print(array[j] + " ");
+            }
         }
 
         else
         {
-            System.out.println("Invalid Input");
+            System.out.println("Invalid Input. Run the program again!!");
         }
     }
 }
