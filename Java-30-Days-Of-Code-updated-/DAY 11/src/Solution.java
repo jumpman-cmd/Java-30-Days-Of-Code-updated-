ac9e9e1658591aa6c2093daf0e5d002c03deaf07
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-20 18:11:59 +0200

    Update Solution.java
    I figured a better solution!

--- Java-30-Days-Of-Code-updated-/DAY 11/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 11/src/Solution.java
@@ -1,39 +1,45 @@
+// Here I thought we were supposed to write 16 array loops to manually traverse.
+// But then I realised we don't know what the user will input into the scanner.
+// On real, practice makes perfect.
 import java.util.Scanner;
 
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
-        Scanner scanner = new Scanner(System.in);
-        int[][] arr = new int[6][6];
-
+        Scanner scan = new Scanner(System.in);
+        int[][] A = new int[6][6];
+        int currentHourglassSum = 0;
+        int maxHourglassSum = Integer.MIN_VALUE;
+        
         for (int i = 0; i < 6; i++)
         {
             for (int j = 0; j < 6; j++)
             {
-                arr[i][j] = scanner.nextInt();
+                A[i][j] = scan.nextInt();
             }
         }
-        scanner.close();
-
-        int maxHourglassSum = Integer.MIN_VALUE;
-
+        maxHourglassSum = Integer.MIN_VALUE;
+        
+        //  Traverse possible hourglass Sum
         for (int i = 0; i <= 3; i++)
         {
             for (int j = 0; j <= 3; j++)
             {
-                int hourglassSum =
-                        arr[i][j] + arr[i][j+1] + arr[i][j+2] +
-                                arr[i+1][j+1] +
-                                arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
-
-                if (hourglassSum > maxHourglassSum)
+                currentHourglassSum = A[i][j] + A[i][j + 1] + A[i][j + 2]
+                + A[i + 1][j + 1]
+                + A[i + 2][j] + A[i + 2][j + 1] + A[i + 2][j + 2];
+                
+                // update the max hour glass sum
+                if (currentHourglassSum > maxHourglassSum)
                 {
-                    maxHourglassSum = hourglassSum;
+                    maxHourglassSum = currentHourglassSum;
                 }
             }
         }
-
+        
         System.out.println(maxHourglassSum);
+        
+        scan.close();
     }
 }
