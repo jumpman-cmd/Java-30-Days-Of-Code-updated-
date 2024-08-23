e8cad8b74176f7174429966c6f4dd0ee76bfdc3c
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-23 20:26:49 +0200

    Update Solution.java

--- Java-30-Days-Of-Code-updated-/DAY 16/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 16/src/Solution.java
@@ -1,23 +1,34 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Solution
+
+
+public class Solution 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException 
     {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        scanner.close();
-
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String S = bufferedReader.readLine();
+        
         try
         {
-            int number = Integer.parseInt(input);
-            System.out.println(number);
+            int number = Integer.parseInt(S.trim());
+            System.out.println(S);
         }
-
-        catch (NumberFormatException e)
+        
+        catch(NumberFormatException nfe)
         {
-            System.out.println("Bad String");
+            System.out.println("Bad String"); // or write - System.out.println("Bad String" + nfe.getMessage());
         }
+        bufferedReader.close();
     }
 }
