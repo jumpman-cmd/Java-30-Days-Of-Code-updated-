7c3e7b9a925b4097895688829d7547a7a03a7ccd
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-23 17:36:55 +0200

    Update Solution.java

--- Java-30-Days-Of-Code-updated-/DAY 15/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 15/src/Solution.java
@@ -1,60 +1,81 @@
 import java.io.*;
 import java.util.*;
 
-class Node
+class Node 
 {
     int data;
     Node next;
-
-    Node(int d)
+    
+    Node(int d) 
     {
         data = d;
         next = null;
     }
+    
+    public int getData()
+    {
+        return data;
+    }
+    
+    public Node getNext()
+    {
+        return next;
+    }
+    
+    public void setData(int newData)
+    {
+        this.data = newData;
+    }
+    
+    public void setNext(Node newNode)
+    {
+        this.next = newNode;
+    }
 }
 
-class Solution
+class Solution 
 {
+    Node head;
+    
     public static Node insert(Node head, int data)
     {
         Node newNode = new Node(data);
         if (head == null)
         {
-            head = newNode;
-        }
-
-        else
+            return newNode;
+        } 
+        else 
         {
             Node current = head;
-            while (current.next != null)
+            while (current.getNext() != null)
             {
-                current = current.next;
+                current = current.getNext();
             }
-            current.next = newNode;
+            current.setNext(newNode);
+            return head;
         }
-        return head;
     }
 
-    public static void display(Node head)
+    public static void display(Node head) 
     {
         Node start = head;
-        while(start != null)
+        while(start != null) 
         {
             System.out.print(start.data + " ");
             start = start.next;
         }
     }
 
-    public static void main(String args[])
+    public static void main(String args[]) 
     {
         Scanner sc = new Scanner(System.in);
         Node head = null;
         int N = sc.nextInt();
 
-        while(N-- > 0)
+        while(N-- > 0) 
         {
             int ele = sc.nextInt();
-            head = insert(head,ele);
+            head = insert(head, ele);
         }
         display(head);
         sc.close();
