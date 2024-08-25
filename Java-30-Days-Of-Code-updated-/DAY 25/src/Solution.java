19e723209961159a05117768c2f408b140ac95a7
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-25 20:07:08 +0200

    Update Solution.java

--- Java-30-Days-Of-Code-updated-/DAY 25/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 25/src/Solution.java
@@ -1,50 +1,45 @@
+// Running Time and Complexity
+
 import java.util.Scanner;
 
-public class Solution
+public class Solution 
 {
-    public static boolean isPrime(int n)
-    {
-        if (n <= 1)
-        {
-            return false;
-        }
-
-        if (n <= 3)
-        {
-            return true;
-        }
-
-        if (n % 2 == 0 || n % 3 == 0)
-        {
-            return false;
-        }
-
-        for (int i = 5; i * i <= n; i += 6)
-        {
-            if (n % i == 0 || n % (i + 2) == 0)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        for (int i = 0; i < T; i++)
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        
+        while(T-- > 0)
         {
-            int n = scanner.nextInt();
-            if (isPrime(n)) {
-                System.out.println("Prime");
-            }
-
-            else
+            int n = scan.nextInt();
+            if(n == 1 || n < 1)
             {
                 System.out.println("Not prime");
             }
+            
+            else
+            {
+                boolean prime = true;
+                for(int i = 2; i * i <= n; i++)
+                {
+                    if(n % i == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                
+                if(prime)
+                {
+                    System.out.println("Prime");
+                }
+                
+                else
+                {
+                    System.out.println("Not prime");
+                }
+            }
         }
-        scanner.close();
     }
 }
