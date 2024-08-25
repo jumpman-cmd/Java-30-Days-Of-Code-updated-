028ee7f1ce119517ecc75bb712952737668e7162
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-25 19:09:37 +0200

    Update Solution.java

--- Java-30-Days-Of-Code-updated-/DAY 23/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 23/Solution.java
@@ -1,81 +1,64 @@
 import java.util.*;
-
-class Node 
-{
-    Node left, right;
+import java.io.*;
+class Node{
+    Node left,right;
     int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
 
-    public Node(int data)
-     {
-        this.data = data;
-        left = right = null;
-     }
+static void levelOrder(Node root)
+{
+    //Write your code here
+    
+    Queue<Node> qu = new LinkedList<>();
+    qu.offer(root);
+    
+    while(qu.peek() != null)
+    {
+        Node newNode = qu.poll();
+        System.out.print(newNode.data + " ");
+        
+        if(newNode.left != null)
+        {
+            qu.offer(newNode.left);
+        }
+        
+        if(newNode.right != null)
+        {
+            qu.offer(newNode.right);
+        }
+    }  
 }
 
-class Solution 
-{
-    public static void levelOrder(Node root)
-     {
-        if (root == null)
-         {
-            return;
-         }
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) 
-        {
-            Node current = queue.poll();
-            System.out.print(current.data + " ");
-
-            if (current.left != null)
-             {
-                queue.add(current.left);
-             }
-            if (current.right != null)
-             {
-                queue.add(current.right);
-             }
-        }
-    }
-
-    public static Node insert(Node root, int data)
-    {
-        if (root == null) 
-        {
+public static Node insert(Node root,int data){
+        if(root==null){
             return new Node(data);
-        } 
-        
-        else 
-        {
+        }
+        else{
             Node cur;
-            if (data <= root.data)
-             {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } 
-            
-            else 
-            {
-                cur = insert(root.right, data);
-                root.right = cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
             }
             return root;
         }
     }
-
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Node root = null;
-        while (n-- > 0) 
-        {
-            int data = sc.nextInt();
-            root = insert(root, data);
-        }
-        sc.close();
-        levelOrder(root);
-    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
 }
