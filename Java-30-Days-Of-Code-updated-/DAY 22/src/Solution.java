ce6d7ecbe157ceff489f9be47cbd9000fba1f425
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-25 16:11:29 +0200

    Update Solution.java

--- Java-30-Days-Of-Code-updated-/DAY 22/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 22/src/Solution.java
@@ -1,66 +1,57 @@
-import java.util.Scanner;
-
-class Node
-{
-    Node left, right;
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
     int data;
-
-    public Node(int data)
-    {
-        this.data = data;
-        left = right = null;
+    Node(int data){
+        this.data=data;
+        left=right=null;
     }
 }
+class Solution{
 
-class Solution
-{
-
-    public static int getHeight(Node root)
+	public static int getHeight(Node root)
     {
-        if (root == null)
-        {
-            return -1;
-        }
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
-        return 1 + Math.max(leftHeight, rightHeight);
+      //Write your code here
+      if(root == null)
+      {
+          return -1;
+      }
+      
+      else
+      {
+          int leftHeight = getHeight(root.left);
+          int rightHeight = getHeight(root.right);
+          return Math.max(leftHeight, rightHeight) + 1;
+      }
     }
 
-    public static Node insert(Node root, int data)
-    {
-        if (root == null)
-        {
+    public static Node insert(Node root,int data){
+        if(root==null){
             return new Node(data);
-        } else
-        {
+        }
+        else{
             Node cur;
-            if (data <= root.data)
-            {
-                cur = insert(root.left, data);
-                root.left = cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
             }
-
-            else
-            {
-                cur = insert(root.right, data);
-                root.right = cur;
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
             }
             return root;
         }
     }
-
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Node root = null;
-        while (n-- > 0)
-        {
-            int data = sc.nextInt();
-            root = insert(root, data);
+	 public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
         }
-        sc.close();
-        int height = getHeight(root);
+        int height=getHeight(root);
         System.out.println(height);
     }
 }
