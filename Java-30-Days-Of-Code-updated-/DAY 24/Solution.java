e97567d649c002d18a6fe2bc7bc34607aa7deacb
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-08-25 19:33:21 +0200

    Update Solution.java

--- Java-30-Days-Of-Code-updated-/DAY 24/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 24/Solution.java
@@ -1,84 +1,80 @@
-import java.util.Scanner;
-
-class Node 
-{
-    int data;
-    Node next;
-    Node(int d) 
-    {
-        data = d;
-        next = null;
+import java.io.*;
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+        data=d;
+        next=null;
     }
+	
 }
-
-class Solution 
+class Solution
 {
-    public static Node removeDuplicates(Node head)
-     {
-        if (head == null) return null;
-
-        Node current = head;
-        while (current != null && current.next != null)
-         {
-            if (current.data == current.next.data) 
-            {
-                current.next = current.next.next;
-            } 
-            
-            else 
-            {
-                current = current.next;
-            }
-        }
-        return head;
-    }
-
-    public static Node insert(Node head, int data)
-     {
-        Node p = new Node(data);
+    public static Node removeDuplicates(Node head) 
+    {
+        //Write your code here
+        
         if (head == null)
         {
-            head = p;
-        } 
+            return null;
+        }
         
-        else if (head.next == null) 
+        Node start = head;
+        
+        while(start.next != null)
         {
-            head.next = p;
-        } 
-        
-        else
-         {
-            Node start = head;
-            while (start.next != null)
-             {
+            if(start.data != start.next.data)
+            {
                 start = start.next;
             }
-            start.next = p;
+            
+            else
+            {
+                start.next = start.next.next;
+            }
         }
+        
         return head;
     }
 
-    public static void display(Node head)
+	 public static  Node insert(Node head,int data)
      {
-        Node start = head;
-        while (start != null)
-         {
-            System.out.print(start.data + " ");
-            start = start.next;
-        }
-    }
-
-    public static void main(String args[]) 
-    {
-        Scanner sc = new Scanner(System.in);
-        Node head = null;
-        int T = sc.nextInt();
-        while (T-- > 0) 
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
         {
-            int ele = sc.nextInt();
-            head = insert(head, ele);
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
         }
-        head = removeDuplicates(head);
-        display(head);
+        return head;
     }
-}
+    public static void display(Node head)
+        {
+              Node start=head;
+              while(start!=null)
+              {
+                  System.out.print(start.data+" ");
+                  start=start.next;
+              }
+        }
+        public static void main(String args[])
+        {
+              Scanner sc=new Scanner(System.in);
+              Node head=null;
+              int T=sc.nextInt();
+              while(T-->0){
+                  int ele=sc.nextInt();
+                  head=insert(head,ele);
+              }
+              head=removeDuplicates(head);
+              display(head);
+
+       }
+    }
