663d0e5b8d21ca4f69205e0fe655807161c8713b
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-01 19:17:04 +0200

    Update Solution.java

--- Java-30-Days-Of-Code-updated-/DAY 25/src/Solution.java
+++ Java-30-Days-Of-Code-updated-/DAY 25/src/Solution.java
@@ -1,18 +1,19 @@
-// Running Time and Complexity
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scan = new Scanner(System.in);
-        int T = scan.nextInt();
+        
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
         
         while(T-- > 0)
         {
-            int n = scan.nextInt();
+            int n = sc.nextInt();
+            
             if(n == 1 || n < 1)
             {
                 System.out.println("Not prime");
@@ -21,6 +22,7 @@ public class Solution
             else
             {
                 boolean prime = true;
+                
                 for(int i = 2; i * i <= n; i++)
                 {
                     if(n % i == 0)
@@ -41,5 +43,6 @@ public class Solution
                 }
             }
         }
+        sc.close();
     }
 }
